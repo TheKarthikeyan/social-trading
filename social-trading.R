fe6bcd889be2937e44aa3c9ddf01b126fbb009a4
Karthikeyan Malaisamy <karthikm@Karthikeyans-MacBook-Pro.local>
2019-08-08 15:17:34 +0530

    Data prepared, sets separated and basic poisson model fitted

--- social-trading.R
+++ social-trading.R
@@ -8,10 +8,9 @@ getwd()
 
 library(readxl)
 library(ggplot2)
-library(naniar)
 library(dplyr)
 library(car)
-library(maps)
+library(lmtest)
 
 zt1 <- read_excel("Dataset/Excel/ZuluTrade 2014-04-06 Summary Data.xlsx", na = "N/A")
 zt2 <- read_excel("Dataset/Excel/ZuluTrade Summary Data 2013-07-28.xlsx", na = "N/A")
@@ -34,12 +33,12 @@ zt1$Ranking[is.na(zt1$`Open Positions Pips`)]
 
 zt1 <- zt1[-16828,]
 gg_miss_upset(zt1, nsets = n_var_miss(zt1))
-
+gg_miss_var(zt1,show_pct = TRUE)
 
 plot(zt1$`Pips Profit`)
 fivenum(zt1$`Pips Profit`)
 boxplot(zt1$`Pips Profit`)
-gg_miss_var(zt1,show_pct = TRUE)
+
 
 
 # Checking repeated members in datasets
@@ -57,23 +56,49 @@ length(intersect(zt3$`Provider ID`,zt4$`Provider ID`))
 #========================================================================#
 
 zt1_non_NA <- zt1[,c(2,4:13,16:20,22:24,37:42)]
-
 str(zt1_non_NA)
-zt1_non_NA$Ranking <- factor(zt1_non_NA$Ranking, ordered = T)
-zt1_non_NA$Amount_Following_Log <- log(zt1_non_NA$`Amount Following`)
-zt1_non_NA$`Has Live Followers` <- factor(as.numeric(zt1_non_NA$`Has Live Followers`))
-zt1_non_NA$Viewed_Log <- log(zt1_non_NA$Viewed)
+# zt1_non_NA$Ranking <- factor(zt1_non_NA$Ranking, ordered = T)
+zt1_non_NA$Amount_Following_Log <- log(zt1_non_NA$`Amount Following`+1)
+zt1_non_NA$`Has Live Followers`[zt1_non_NA$`Has Live Followers`=="Yes"] <- 1
+zt1_non_NA$`Has Live Followers`[zt1_non_NA$`Has Live Followers`=="No"] <- 0
+zt1_non_NA$Follow <- vector('numeric', length = dim(zt1_non_NA)[1])
+zt1_non_NA$Follow[zt1_non_NA$Followers>0] <- 1
+zt1_non_NA$Follow[zt1_non_NA$Followers<=0] <- 0
+zt1_non_NA$`Has Live Followers` <- factor(zt1_non_NA$`Has Live Followers`)
+zt1_non_NA$Viewed_Log <- log(zt1_non_NA$Viewed+1)
 zt1_non_NA$`Trading Own Money` <- factor(zt1_non_NA$`Trading Own Money`)
 zt1_non_NA$`Is EA` <- factor(as.numeric(zt1_non_NA$`Is EA`))
 zt1_non_NA$`Economic Event Trade` <- factor(as.numeric(zt1_non_NA$`Economic Event Trade`))
 zt1_non_NA$`Amount Following` <- NULL
 zt1_non_NA$Viewed <- NULL
 zt1_non_NA$`Country ISO Code` <- NULL
-
 #========================================================================#
 # Data Preparation - End                                                 #
 #========================================================================#
 
+#========================================================================#
+# Data Separation - Begin                                                #
+#========================================================================#
+
+set.seed(777)
+
+train.rows <- sample(rownames(zt1_non_NA),dim(zt1_non_NA)[1]*0.8)
+test.rows <- setdiff(rownames(zt1_non_NA),train.rows)
+
+train.data <- zt1_non_NA[train.rows,]
+test.data <- zt1_non_NA[test.rows,]
+
+#========================================================================#
+# Data Separation - End                                                  #
+#========================================================================#
+
+#========================================================================#
+# Model building - Begin                                                 #
+#========================================================================#
+
+model <- glm(Follow ~ ., data=train.data, family = poisson(link = "log"))
 
 
-
+#========================================================================#
+# Model building - End                                                   #
+#========================================================================#
