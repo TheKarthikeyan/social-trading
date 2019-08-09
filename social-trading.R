64cced293aebb10b44a20722ddf94af29dac2a8d
Karthikeyan Malaisamy <karthikm@Karthikeyans-MacBook-Pro.local>
2019-08-10 03:10:38 +0530

    Multi-collinearity check added

--- social-trading.R
+++ social-trading.R
@@ -11,6 +11,7 @@ library(ggplot2)
 library(dplyr)
 library(car)
 library(lmtest)
+library(caret)
 
 zt1 <- read_excel("Dataset/Excel/ZuluTrade 2014-04-06 Summary Data.xlsx", na = "N/A")
 zt2 <- read_excel("Dataset/Excel/ZuluTrade Summary Data 2013-07-28.xlsx", na = "N/A")
@@ -61,14 +62,14 @@ str(zt1_non_NA)
 zt1_non_NA$Amount_Following_Log <- log(zt1_non_NA$`Amount Following`+1)
 zt1_non_NA$`Has Live Followers`[zt1_non_NA$`Has Live Followers`=="Yes"] <- 1
 zt1_non_NA$`Has Live Followers`[zt1_non_NA$`Has Live Followers`=="No"] <- 0
-zt1_non_NA$Follow <- vector('numeric', length = dim(zt1_non_NA)[1])
-zt1_non_NA$Follow[zt1_non_NA$Followers>0] <- 1
-zt1_non_NA$Follow[zt1_non_NA$Followers<=0] <- 0
 zt1_non_NA$`Has Live Followers` <- factor(zt1_non_NA$`Has Live Followers`)
 zt1_non_NA$Viewed_Log <- log(zt1_non_NA$Viewed+1)
 zt1_non_NA$`Trading Own Money` <- factor(zt1_non_NA$`Trading Own Money`)
 zt1_non_NA$`Is EA` <- factor(as.numeric(zt1_non_NA$`Is EA`))
 zt1_non_NA$`Economic Event Trade` <- factor(as.numeric(zt1_non_NA$`Economic Event Trade`))
+zt1_non_NA$Follow <- vector('numeric', length = dim(zt1_non_NA)[1])
+zt1_non_NA$Follow[zt1_non_NA$Followers>0] <- 1
+zt1_non_NA$Follow[zt1_non_NA$Followers<=0] <- 0
 zt1_non_NA$`Amount Following` <- NULL
 zt1_non_NA$Viewed <- NULL
 zt1_non_NA$`Country ISO Code` <- NULL
@@ -96,8 +97,35 @@ test.data <- zt1_non_NA[test.rows,]
 # Model building - Begin                                                 #
 #========================================================================#
 
-model <- glm(Follow ~ ., data=train.data, family = poisson(link = "log"))
+# Some static definition
+cook_distance_threshold <- 1
+gvif_threshold <- 2
+p_val_threshold <- 0.1
 
+# Multi-collinearity check begin
+
+del_v_coloumns <- vector('character')
+
+
+while(TRUE) {
+  ds = train.data[,setdiff(names(train.data),del_v_coloumns)]
+  vfit <- vif(glm(train.data$Follow ~ .,
+                  data = ds,
+                  family = poisson(link = "log")))
+  if(max(vfit)>gvif_threshold) {
+    print(which(vfit==max(vfit)))
+    del_v_coloumns <- append(del_v_coloumns,names(ds)[which(vfit == max(vfit))]) 
+    cat("Removing coloumn ",tail(del_v_coloumns,1)," with VIF:",max(vfit),"\n")
+    print(del_v_coloumns)
+  } else {
+    break;
+  }
+}
+
+# Removing multi-collinear columns
+train.data <- subset(train.data,select = names(train.data)[!names(train.data) %in% del_v_coloumns])
+
+# Multi-collinearity check end
 
 #========================================================================#
 # Model building - End                                                   #
